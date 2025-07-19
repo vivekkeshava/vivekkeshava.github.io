7ad4ab034e619ecc729d40de355b9331a16a4b76
Vivek Keshava <vivek.keshava1@gmail.com>
2025-07-19 13:34:03 -0700

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === "production" ? "/vivek-portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/vivek-portfolio/" : "",
+  }
 }
 
 export default nextConfig
